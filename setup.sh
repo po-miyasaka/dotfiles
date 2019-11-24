1495a2528af4f33ad1d659222861765a40f83356
po_miyasaka <miyasaka@yahoo.co.jp>
2019-11-25 02:16:48 +0900

    gitconfig

--- setup.sh
+++ setup.sh
@@ -3,7 +3,7 @@ cd `dirname $0`
 set -e
 
 downloadTools () {
-    if [[ $+commands[brew] ]]; then 
+    if [[ ! $+commands[brew] ]]; then 
         echo brew入れてくれ
         exit 0
     fi
@@ -12,19 +12,20 @@ downloadTools () {
 setupDotFiles () {
     for f in  `echo .*`;
     do
-        if [[ $f = "." ]]; then continue ;fi
+        if [[ $f = "." ]]; then continue; fi
         if [[ $f = ".." ]]; then continue; fi
         if [[ $f = ".DS_Store" ]]; then continue; fi 
         if [[ $f = ".gitignore" ]]; then continue; fi 
         if [[ $f = "setup.sh" ]]; then continue; fi 
         if [[ $f = ".git" ]]; then continue; fi 
-        if [[ $f = ".gitconfigbk" ]]; then　cp "$HOME/dotfiles/$f" "$HOME/.gitconfig"; fi 
+        if [[ $f = ".gitconfigbk"  ]] ; then 
+            cp "$HOME/dotfiles/$f" "$HOME/.gitconfig"
+            continue
+        fi 
 
         ln -sf "$HOME/dotfiles/$f" "$HOME/$f"
     done
 }
-
-
 
 downloadTools
 setupDotFiles
