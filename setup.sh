6e79ca666e1904292e2a4d379f10c3d4c0e0fbfb
po_miyasaka <miyasaka@yahoo.co.jp>
2019-11-25 01:54:54 +0900

    brew があるか確かめる

--- setup.sh
+++ setup.sh
@@ -1,18 +1,30 @@
 PWD="$PWD"
 cd `dirname $0`
-for f in  `echo .*`;
-do
-    if [[ $f = "." ]]; then continue ;fi
-    if [[ $f = ".." ]]; then continue; fi
-    if [[ $f = ".DS_Store" ]]; then continue; fi 
-    if [[ $f = ".gitignore" ]]; then continue; fi 
-    if [[ $f = "setup.sh" ]]; then continue; fi 
-    if [[ $f = ".git" ]]; then continue; fi 
-    if [[ $f = ".gitconfigbk" ]]; then
-        mv "$HOME/dotfiles/$f" "$HOME/.gitconfig"
-    fi 
+set -e
 
-    ln -sf "$HOME/dotfiles/$f" "$HOME/$f"
-done
+downloadTools () {
+    if [[ $+commands[brew] ]]; then 
+        echo brew入れてくれ
+        exit 0
+    fi
+}
 
-source "$HOME/.zprofile"
+setupDotFiles () {
+    for f in  `echo .*`;
+    do
+        if [[ $f = "." ]]; then continue ;fi
+        if [[ $f = ".." ]]; then continue; fi
+        if [[ $f = ".DS_Store" ]]; then continue; fi 
+        if [[ $f = ".gitignore" ]]; then continue; fi 
+        if [[ $f = "setup.sh" ]]; then continue; fi 
+        if [[ $f = ".git" ]]; then continue; fi 
+        if [[ $f = ".gitconfigbk" ]]; then　mv "$HOME/dotfiles/$f" "$HOME/.gitconfig"; fi 
+
+        ln -sf "$HOME/dotfiles/$f" "$HOME/$f"
+    done
+}
+
+
+
+downloadTools
+setupDotFiles
